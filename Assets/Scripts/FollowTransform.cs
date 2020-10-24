93da3bd6768258f86633cd204ba89a3b7f9ec790
Stefano Palma <stefano.palma1@studenti.unimi.it>
2020-10-24 16:10:12 +0200

    Snow

--- Assets/Scripts/FollowTransform.cs
+++ Assets/Scripts/FollowTransform.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class FollowTransform : MonoBehaviour
 {
+    public Transform targetTransform;
+    public float speed;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class FollowTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, targetTransform.position + offset, speed);
     }
 }
