db0b5a4b292a9ee6b0d7c5a837fea3867c53293e
Stefano Palma <stefano.palma1@studenti.unimi.it>
2021-05-08 16:17:19 +0200

    Main menu joypad, audio + music scripts and review first 4 descriptions

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    public static AudioManager Instance { get { return _instance; } }
+
+    public AudioSource soundSource;
+    public AudioSource musicSource;
+
+    public AudioClip[] sounds;
+    public string[] soundNames;
+    public AudioClip[] musics;
+    public string[] musicNames;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getSoundIndex(string sound)
+    {
+        int index = 0;
+        for (int i = 0; i < soundNames.Length; i++)
+        {
+            if (soundNames[i] == sound)
+            {
+                index = i;
+                i = soundNames.Length;
+            }
+        }
+        return index;
+    }
+
+    public int getMusicIndex(string music)
+    {
+        int index = 0;
+        for (int i = 0; i < musicNames.Length; i++)
+        {
+            if (musicNames[i] == music)
+            {
+                index = i;
+                i = musicNames.Length;
+            }
+        }
+        return index;
+    }
+
+    public void PlaySound(string name)
+    {
+        int index = getSoundIndex(name);
+        soundSource.PlayOneShot(sounds[index]);
+    }
+
+    public void PlayMusic(string name)
+    {
+        int index = getMusicIndex(name);
+        musicSource.clip = musics[index];
+        musicSource.Play();
+    }
+}
---
+++ Assets/Scripts/MainMenuJoypad.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuJoypad : MonoBehaviour
+{
+    public Button firstButton;
+    public Button secondButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetAxis("Vertical") < 0.02f)
+        {
+            firstButton.Select();
+        }
+        else if(Input.GetAxis("Vertical") > 0.02f)
+        {
+            secondButton.Select();
+        }
+    }
+}
